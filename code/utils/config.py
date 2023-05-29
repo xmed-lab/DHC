de69fa4924c5336e9175dba473c5d5846fdb02c6
Haonan Wang <haonan1wang@gmail.com>
2023-05-29 16:01:37 +0800

    Add files via upload

--- code/utils/config.py
+++ code/utils/config.py
@@ -2,7 +2,7 @@
 class Config:
     def __init__(self,task):
         if task == "synapse":
-            self.base_dir = '/home/xmli/hnwang/Datasets/Synapse'
+            self.base_dir = './Datasets/Synapse'
             self.save_dir = './synapse_data'
             self.patch_size = (64, 128, 128)
             self.num_cls = 14
@@ -10,7 +10,7 @@ class Config:
             self.n_filters = 32
             self.early_stop_patience = 100
         else: # amos
-            self.base_dir = '/home/xmli/hnwang/Datasets/amos22'
+            self.base_dir = './Datasets/amos22'
             self.save_dir = './amos_data'
             self.patch_size = (64, 128, 128)
             self.num_cls = 16
